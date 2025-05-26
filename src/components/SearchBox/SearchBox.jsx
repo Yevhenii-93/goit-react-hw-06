4585f8137f1cc1c3e96742cfe9b1789149589578
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-05-26 20:01:48 +0300

    fix debounce filter

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,22 +1,13 @@
 import css from "./SearchBar.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { changeFilter } from "../../redux/filtersSlice";
-import { useEffect, useState } from "react";
-import { useDebounce } from "use-debounce";
 
 export default function SearchBar() {
   const filter = useSelector((state) => state.filters.name);
   const dispatch = useDispatch();
 
-  const [value, setValue] = useState("");
-  const [debouncedValue] = useDebounce(value, 1000);
-
-  useEffect(() => {
-    dispatch(changeFilter(debouncedValue));
-  }, [debouncedValue, dispatch]);
-
   const handleChange = (e) => {
-    setValue(e.target.value);
+    dispatch(changeFilter(e.target.value));
   };
 
   return (
