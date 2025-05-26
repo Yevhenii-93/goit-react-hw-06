1fc9e330c6af1ee06b5ae313aca189f490e4d607
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-05-26 21:17:54 +0300

    deleted debounce and fix code

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,13 @@
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
-import { useDebounce } from "use-debounce";
 
 export default function ContactList() {
   const contacts = useSelector((state) => state.contacts.items);
   const filter = useSelector((state) => state.filters.name.toLowerCase());
-  const [debounceFilter] = useDebounce(filter.toLowerCase(), 1000);
 
   const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(debounceFilter)
+    contact.name.toLowerCase().includes(filter)
   );
 
   return (
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,7 +2,7 @@ import css from "./SearchBar.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { changeFilter } from "../../redux/filtersSlice";
 
-export default function SearchBar() {
+export default function SearchBox() {
   const filter = useSelector((state) => state.filters.name);
   const dispatch = useDispatch();
 
